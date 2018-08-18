fbdd9a675c25ab5b0ae8df2aa5532870105a182b
Ogamiq <miloszogaza@op.pl>
2018-08-18 21:13:10 +0200

    simplified api url in basic info controller

--- scripts/BasicInfoController.js
+++ scripts/BasicInfoController.js
@@ -2,8 +2,7 @@ class BasicInfoController {
     constructor() {
         this.CONFING = {
             API_KEY: "217fefe7c589b896f33d0970f7b9d3e0",
-            CITY_ID: 3081368,
-            API_URL: "api.openweathermap.org/data/2.5/weather?id=${CITY_ID}"
+            API_URL: "api.openweathermap.org/data/2.5/weather?id=3081368"
         }
     }  
 }
