0ff3181540c05c22436b9207f135d2f5a4588449
Ogamiq <miloszogaza@op.pl>
2018-08-18 14:05:02 +0200

    added files for section basic info

---
+++ scripts/BasicInfo.js
@@ -0,0 +1,6 @@
+class BasicInfo {
+    constructor() {
+        this.descriptionL = document.querySelectorAll('basic-info-boxL basic-info-description');
+        this.descriptionR = document.querySelectorAll('basic-info-boxR basic-info-description');
+    }
+}
---
+++ scripts/BasicInfoController.js
@@ -0,0 +1,5 @@
+const CONFING = {
+    API_KEY: "217fefe7c589b896f33d0970f7b9d3e0",
+    CITY_ID: 3081368,
+    API_URL: "api.openweathermap.org/data/2.5/weather?id=${CITY_ID}",
+}
