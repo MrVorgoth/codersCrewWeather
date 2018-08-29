90d822d82245178e74495e92f3b9ba7b5a51bb77
Ogamiq <miloszogaza@op.pl>
2018-08-29 17:36:03 +0200

    removed unused import statement.

--- src/js/render.js
+++ src/js/render.js
@@ -1,6 +1,4 @@
-import BasicInfo from './BasicInfo.js';
 import BasicInfoController from './BasicInfoController.js';
-
 
 window.onload = () => {
     let basicInfoController = new BasicInfoController();
