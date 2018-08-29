523b41f90ba3caf309cb11de9d376ba109687146
Ogamiq <miloszogaza@op.pl>
2018-08-29 17:24:05 +0200

    removed load data from api button, data loads automatically now.

--- src/js/render.js
+++ src/js/render.js
@@ -5,6 +5,5 @@ import BasicInfoController from './BasicInfoController.js';
 window.onload = () => {
     let basicInfoController = new BasicInfoController();
     let renderHTML = basicInfoController.renderHTML.bind(basicInfoController);
-    let buttonLoadData = document.querySelector('#buttonLoadData');
-    buttonLoadData.addEventListener("click",renderHTML);
+    renderHTML();
 }
