8eb3badad3077c93672ba65c63ec089ec35e175d
Łukasz Królak <lukasz.krolak99@gmail.com>
2018-08-30 09:56:15 +0200

    Added chart css

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ require("../../index.html");
 require("../css/style.css");
 require("../css/basic-info.css");
 require("../css/weather-list.css");
+require("../css/chart.css");
 require("./render.js");
 require("./weather-list.js");
 require("./chart.js");
